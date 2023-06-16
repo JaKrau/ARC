68f84f4e415507cb586967a4524266ae66eeb9fe
JaKrau <jakeziah@gmail.com>
2023-06-16 09:04:05 -0700

    add JS to mainScript functions displayDay/Episode

--- Assets/Scripts/mainScript.js
+++ Assets/Scripts/mainScript.js
@@ -22,7 +22,7 @@ class Episode{
 /*
 Working variables
 */
-
+var now = dayjs().format("M/DD");
 var favorites = new Array();
 var episodes = [new Array(), new Array(), new Array(), new Array()];
 
@@ -133,10 +133,30 @@ function displayList(){
     <article class="episode"></article>
 </section>
 */
-function displayDay(day){
 
+
+function displayDay(day){
+  var day0 = document.getElementById('day-0');
+  var day1 = document.getElementById('day-1');
+  var day2 = document.getElementById('day-2');
+  var day3 = document.getElementById('day-3');
+
+  var yesterday = now.subtract(1, 'day');
+  var twoDaysAgo = now.subtract(2, 'day');
+  var threeDaysAgo = now.subtract(3, 'day');
+
+  day0.textContent = "Today " + "(" + now + ")";
+  day1.textContent = "Yesterday " + "(" + yesterday + ")";
+  day2.textContent = "Two Days Ago " + "(" + twoDaysAgo + ")";
+  day3.textContent = "Three Days Ago " + "(" + threeDaysAgo + ")";
+
+  day0.innerHTML = fiveEpisodeDisplay; 
+  day1.innerHTML = fiveEpisodeDisplay;
+  day2.innerHTML = fiveEpisodeDisplay;
+  day3.innerHTML = fiveEpisodeDisplay;
 }
 
+displayDay();
 //Creates Dom elements, and adds information from episodes[day][key] returns Dom Element Object for an episode
 /*
 <article class="episode">
@@ -150,14 +170,37 @@ function displayDay(day){
 </article>
 */
 function displayEpisode(day, key){
+  let sectionEl = document.getElementsByClassName("day");
+  
   let articleEl = document.createElement("article");
   articleEl.className = "episode";
+  
   let aElement1 = document.createElement("a");
-  aElement1.setAttribute("href=episodes[day][key].seriesLink");
-  aElement1.innerHTML("<h3>episodes[day][key].series</h3>");
+  aElement1.setAttribute("href=episodes[day][key].seriesLink", "class=itemList");
+  let h3 = document.createElement("h3");  
+  h3.innerHTML = episodes[day][key].series;
+  
   let aElement2 = document.createElement("a");
   aElement2.setAttribute("href=episodes[day][key].link");
-  aElement2.innerHTML("<h2\"Ep.\" + episodes[day][key].number + \" \" + episodes[day][key].title</h2>", "<img src=episodes[day][key].preview />");
+  let h2 = document.createElement("h2");
+  h2.innerHTML = "Ep." + episodes[day][key].number + " " + episodes[day][key].title;
+  
+  let episodeImage = document.createElement("img");
+  episodeImage.setAttribute("class = episodeImage");
+  episodeImage.setAttribute("src=episodes[day][key].preview");
+  
+  let releaseDate = document.createElement("h4");  
+  releaseDate.setAttribute("class = releaseDate");
+  releaseDate.innerHTML = episodes[day][key].release;
+
+  let lineBreak = document.createElement("br");
+
+  aElement1.appendChild(h3);
+  aElement2.appendChild(h2, h4, lineBreak);
+  aElement2.appendChild(episodeImage);
   articleEl.appendChild(aElement1);
   articleEl.appendChild(aElement2);
+  sectionEl.appendChild(articleEl);
 }
+
+let fiveEpisodeDisplay = [displayEpisode(), displayEpisode(), displayEpisode(), displayEpisode(), displayEpisode()];
